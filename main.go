734155510375c6e02b7cab89ac60cdaea216e8b6
ghaidafasya24 <ghaidafasya5@gmail.com>
2024-06-20 20:06:35 +0700

    swagger

--- main.go
+++ main.go
@@ -11,8 +11,23 @@ import (
 
 	"github.com/ghaidafasya24/ws-ghaida2024/url"
 
+
+	_ "github.com/ghaidafasya24/ws-ghaida2024/docs"
 	"github.com/gofiber/fiber/v2"
+	
 )
+
+// @title TES SWAGGER ULBI
+// @version 1.0
+// @description This is a sample swagger for Fiber
+
+// @contact.name API Support
+// @contact.url https://github.com/ghaidafasya24
+// @contact.email 714220031@std.ulbi.ac.id
+
+// @host ws-ghaida2024-10da574be531.herokuapp.com
+// @BasePath /
+// @schemes https http
 
 func main() {
 	site := fiber.New(config.Iteung)
